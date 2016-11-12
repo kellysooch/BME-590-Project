8ec96e90f5154a9606593e43bf1f440941cb7c74
Filip Mazurek <fjm7@duke.edu>
2016-11-12 18:14:23 -0500

    Adds basic project framework, API.

---
+++ ImageOutputter.py
@@ -0,0 +1,11 @@
+class ImageOutputter:
+
+    def render_image(self):
+        # renders
+
+    def save_image(self):
+        # saves
+
+class ImageMaker:
+
+    def make_image(self):
---
+++ SignalManipulator.py
@@ -0,0 +1,13 @@
+class SignalManipulator:
+
+    def get_pretty_signal(self):
+        # apply LPF
+        # apply log compression
+        # apply compensation for distance
+        # apply compensation for harmonic interactions
+        return # pretty signal
+
+    def low_pass_filter(self):
+        # apply envelope detection
+
+    def log_compress_signal(self):
