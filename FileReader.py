8fbe360a337b4cc417cd7a51d599f6f98b270635
kas100 <kas100@duke.edu>
2016-11-14 18:56:57 -0500

    wrote functions in JSON Reader and Binary Reader classes, and called their outputs on main.py

--- FileReader.py
+++ FileReader.py
@@ -1,11 +1,31 @@
 class JSONReader:
 
-    def read_json(self):
-        return #class that is Metadata
+    def __init__(self,fs, c, axial_samples, num_beams, beam_spacing):
+        self.fs = fs
+        self.c = c
+        self.axial_samples = axial_samples
+        self.num_beams = num_beams
+        self.beam_spacing = beam_spacing
+
+    def read_json(filename):
+        import json
+        f = open(filename)
+        data = json.load(f)
+        fs = data["fs"]
+        c = data["c"]
+        axial_samples = data["axial_samples"]
+        num_beams = data["num_beams"]
+        beam_spacing = data["beam_spacing"]
+        objects = fs, c, axial_samples, num_beams, beam_spacing
+        return objects
 
 class BinaryReader:
 
-    def read_binary(self):
+    def __init__(self, data):
+        self.data = data
+
+    def read_binary(filename):
+        import numpy as np
         f = open(filename, 'rb')
         data = np.fromfile(f,'uint16')
         return data
--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
-if __name__ == "__main__":
-
+# if __name__ == "__main__":
+from FileReader import JSONReader, BinaryReader
+objects = JSONReader.read_json("bmode.json") # objects contain fs, c, axial_samples, num_beams, beam_spacing
+data = BinaryReader.read_binary("rfdat.bin") # contains array of data converted from binary to int
     # read data
     # pretty up the data
     # make image
