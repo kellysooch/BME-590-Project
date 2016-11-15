464d5213e80b1b32a0eb805ab65562d9e13a512f
kas100 <kas100@duke.edu>
2016-11-15 13:33:47 -0500

    added argparse function to specify input filename in Main.py, and changed structure of JSON reader class to return the individual parameters

--- FileReader.py
+++ FileReader.py
@@ -1,31 +1,20 @@
 class JSONReader:
 
-    def __init__(self,fs, c, axial_samples, num_beams, beam_spacing):
-        self.fs = fs
-        self.c = c
-        self.axial_samples = axial_samples
-        self.num_beams = num_beams
-        self.beam_spacing = beam_spacing
-
-    def read_json(filename):
+    def __init__(self, JSONfile):
         import json
-        f = open(filename)
+        f = open(JSONfile)
         data = json.load(f)
-        fs = data["fs"]
-        c = data["c"]
-        axial_samples = data["axial_samples"]
-        num_beams = data["num_beams"]
-        beam_spacing = data["beam_spacing"]
-        objects = fs, c, axial_samples, num_beams, beam_spacing
-        return objects
+
+        self.fs = data["fs"]
+        self.c = data["c"]
+        self.axial_samples = data["axial_samples"]
+        self.num_beams = data["num_beams"]
+        self.beam_spacing = data["beam_spacing"]
 
 class BinaryReader:
 
-    def __init__(self, data):
-        self.data = data
-
-    def read_binary(filename):
+    def __init__(self, filename):
         import numpy as np
         f = open(filename, 'rb')
-        data = np.fromfile(f,'uint16')
-        return data
+        self.data = np.fromfile(f, 'uint16')
+
