e875439eb802ebeff5f2246b1bfb6130d8f2f85c
kas100 <kas100@duke.edu>
2016-11-18 15:52:46 -0500

    -m "added docstrings and save/render options, and added logging to ImageOutputter, Close #7, Close #9, Close #21, Close #26"

--- ImageOutputter.py
+++ ImageOutputter.py
@@ -2,6 +2,22 @@ import logging
 logging.getLogger('ultrasound_kas100_fjm7')
 
 def make_image(data_in_beams, fs, c, axial_samples, num_beams, beam_spacing, do_save, do_display):
+    """
+    Creates Ultrasound image using pcolormesh, taking as inputs the data in beams and the parameters from the JSON file,
+    as well as allowing the user to specify whether they want to save, display, or save and display image. The function
+    first creates a 2d array of the data, with the columns as beams and the rows as axial depth. Then, the meshgrid is
+    created, manipulating the axes such that axial depth will be the y-axis, and lateral distance will be the x-axis.
+
+    :param data_in_beams: data as list of list of data points in a single beam
+    :param fs: sampling frequency (Hz)
+    :param c: sound speed (m/s)
+    :param axial_samples: number of samples in beam
+    :param num_beams: number of lateral beams
+    :param beam_spacing: space between beams
+    :param do_save: Boolean logic if user wants to save image to png
+    :param do_display: Boolean logic if user wants to display image
+    :return: a displayed and/or saved image
+    """
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -21,6 +37,9 @@ def make_image(data_in_beams, fs, c, axial_samples, num_beams, beam_spacing, do_
     plt.pcolormesh(xx, yy, Z)
 
     if do_save:
+        logging.debug('saving image')
         plt.savefig('image.png', format='png')
     if do_display:
+        logging.debug('displaying image')
         plt.show()
+
