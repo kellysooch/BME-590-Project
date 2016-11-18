23e5ad993f239a2067f858bb55ebd85750989f20
kas100 <kas100@duke.edu>
2016-11-18 15:41:55 -0500

    fixed bug in creating 2d array of data

--- ImageOutputter.py
+++ ImageOutputter.py
@@ -1,17 +1,22 @@
+import logging
+logging.getLogger('ultrasound_kas100_fjm7')
 
 def make_image(data_in_beams, fs, c, axial_samples, num_beams, beam_spacing, do_save, do_display):
-    # manipulate axes
     import numpy as np
     import matplotlib.pyplot as plt
 
+    logging.debug('putting data into 2d array')
     Matrix = np.zeros(num_beams, axial_samples)
-    for beam in data_in_beams:
-        for point in beam:
-            Matrix[beam.index()][point.index()] = point
+    for i, beam in enumerate(data_in_beams):
+        for j,point in enumerate(beam):
+            Matrix[i][j] = point
 
+    logging.debug('setting axes, x=Lateral, y=Axial')
     X = np.linspace(0, beam_spacing*num_beams, num_beams)
     Y = np.linspace(0, (axial_samples/fs)*c, axial_samples)
     Z = Matrix
+
+    logging.debug('creating Ultrasound image with pcolormesh')
     xx, yy = np.meshgrid(X, Y)
     plt.pcolormesh(xx, yy, Z)
 
