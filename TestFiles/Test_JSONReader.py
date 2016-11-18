766d8dabd45f139f4bfb715a008418ad0194ad30
kas100 <kas100@duke.edu>
2016-11-18 15:04:50 -0500

    fixed FileReader to read int16 instead of uint16, created ultrasound image and manipulated axes accordingly, Close #6, also added boolean logic to save and/or display image, Close #8

--- TestFiles/Test_JSONReader.py
+++ TestFiles/Test_JSONReader.py
@@ -1,5 +1,9 @@
 
 def test_read_json():
     from FileReader import JSONReader
-    import numpy as np
     assert JSONReader("bmode.json").fs == 40000000
+    assert JSONReader("bmode.json").c == 1540
+    assert JSONReader("bmode.json").axial_samples == 1556
+    assert JSONReader("bmode.json").beam_spacing == 0.00011746274509803921
+    assert JSONReader("bmode.json").num_beams == 256
+
