22d06848396e679814906c3773118477f04c5f23
Filip Mazurek <fjm7@duke.edu>
2016-11-18 01:24:05 -0500

    Adds documentation to SignalManipulator. This closes #23.

--- SignalManipulator.py
+++ SignalManipulator.py
@@ -1,4 +1,10 @@
 def get_pretty_signal(multiple_beams):
+    """ Manipulates the given multiple beams to perform envelope detection and otherwise manipulate the signal to be
+    in a form which the display may take to display in the best form possible
+
+    :param multiple_beams: data as list of list of data points
+    :return: list of list of data points. Now in desired manipulated form
+    """
     multiple_envelopes = []
 
     for single_beam in multiple_beams:
@@ -14,18 +20,34 @@ def get_pretty_signal(multiple_beams):
 
 
 def determine_window_size(rectified_signal):
+    """ Determines a good window size to be used for the moving average function in detecting the envelope.
+
+    :param rectified_signal: single beam, absolute value
+    :return: returns the number of samples between peaks in rectified signal
+    """
     first_peak = first_peak_detect(rectified_signal)
     second_peak = first_peak_detect(rectified_signal[(first_peak+1):])
     return second_peak - first_peak
 
 
 def first_peak_detect(beam):
+    """ Finds the first peak point in the signal.
+
+    :param beam: single signal
+    :return: location of first peak
+    """
     for i in range(1, len(beam)):
         if beam[i-1] < beam[i] < beam[i+1]:
             return i
 
 
 def envelope_detect(rectified_signal, sample_window):
+    """ Applies a moving average to the given signal. This is effectively a low-pass filter.
+
+    :param rectified_signal: single beam, absolute value
+    :param sample_window: desired moving average window
+    :return: signal after envelope detection algorithm
+    """
     envelope = []
     first_total = 0
     for data in rectified_signal[:sample_window]:
@@ -46,6 +68,13 @@ def envelope_detect(rectified_signal, sample_window):
 
 
 def get_envelope_front_pad(rectified_signal, sample_window):
+    """ Returns first few points which are not able to be created with the full moving average average algorithm.
+    Does so by continually averaging the given points.
+
+    :param rectified_signal: single beam, absolute value
+    :param sample_window: desired moving average window
+    :return: missing points to pad the begging of the envelope detection points
+    """
     front_pad = []
     current_avg = 0
 
@@ -60,4 +89,11 @@ def get_envelope_front_pad(rectified_signal, sample_window):
 
 
 def get_envelope_back_pad(rectified_signal, sample_window):
+    """ Returns last few points which are not able to be created with the full moving average average algorithm.
+    Does so by continually averaging the given points.
+
+    :param rectified_signal: single beam, absolute value
+    :param sample_window: desired moving average window
+    :return: missing points to pad the end of the envelope detection points
+    """
     return get_envelope_front_pad(rectified_signal.reverse(), sample_window).reverse()
